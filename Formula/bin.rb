358cb0165b58123b01fb3686c64f9644620cd48d
Paweł Smolak <smolak.paul@gmail.com>
2023-09-04 18:19:58 +0200

    fix bin revision

--- Formula/bin.rb
+++ Formula/bin.rb
@@ -1,7 +1,7 @@
 class Bin < Formula
   desc "Personal scripts"
   homepage "https://github.com/psmolak/bin"
-  url "https://github.com/psmolak/bin.git", tag: "v0.2.3", revision: "929054bb5b97deb263e4f26096226a0375ec6747"
+  url "https://github.com/psmolak/bin.git", tag: "v0.2.3", revision: "4f06839c9a0d6ba4bc8bd371d44b0e9814e2f721"
   license "MIT"
 
   head "https://github.com/psmolak/bin.git", branch: "master"
